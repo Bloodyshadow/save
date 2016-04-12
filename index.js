2cd61a1461ac32b727e9703ad0d67c82b81a280f
FlameShadowV <472703107@qq.com>
2016-04-12 16:28:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,56 @@ module.exports = function(app) {
     });
   });
 
+  app.get('/login', checkNotLogin);
 	app.get('/login', function(req, res) {
-    res.render('reg', {
-      title: '登陸',
+    res.render('login', {
+      title: '登錄',
     });
   });
 
+app.post('/login', checkNotLogin);
+  app.post('/login', function(req, res) {
+    //生成口令的散列值
+    var md5 = crypto.createHash('md5');
+    var password = md5.update(req.body.password).digest('base64');
+    
+    User.get(req.body.username, function(err, user) {
+      if (!user) {
+        req.flash('error', '用戶不存在');
+        return res.redirect('/login');
+      }
+      if (user.password != password) {
+        req.flash('error', '用戶口令錯誤');
+        return res.redirect('/login');
+      }
+      req.session.user = user;
+      req.flash('success', '登入成功');
+      res.redirect('/');
+    });
+  });
+
+  app.get('/logout', checkLogin);
+  app.get('/logout', function(req, res) {
+    req.session.user = null;
+    req.flash('success', '登出成功');
+    res.redirect('/');
+  });
+  
+
+
 	}
+  function checkLogin(req, res, next) {
+  if (!req.session.user) {
+    req.flash('error', '未登入');
+    return res.redirect('/login');
+  }
+  next();
+}
+
+function checkNotLogin(req, res, next) {
+  if (req.session.user) {
+    req.flash('error', '已登入');
+    return res.redirect('/');
+  }
+  next();
+}

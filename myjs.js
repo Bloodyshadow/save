97c53c81c88bbcd529dc9e21bcef963ca186dce2
FlameShadowV <472703107@qq.com>
2016-04-20 11:38:32 +0800

    Create myjs.js

---
+++ myjs.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.choice').click(function(event){
+		$('table').empty();
+})
+	$('#pointone').click(function(event){
+		$('table').append('<label>eee</label><input type="text"></input><button>v</button>');
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
